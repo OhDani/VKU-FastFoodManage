c075a833c1a0a620687593361244988bb5356b2f
OhDani <111127874+OhDani@users.noreply.github.com>
2023-02-27 00:38:46 +0700

    Update FastFoodManagement.java

--- FastFoodStore/src/main/FastFoodManagement.java
+++ FastFoodStore/src/main/FastFoodManagement.java
@@ -7,9 +7,8 @@ import java.sql.Connection;
 
 public class FastFoodManagement {
     public static void main(String[] args) {
-//        Login lo= new Login("QUẢN LÍ CỬA HÀNG ĐỒ ĂN NHANH");
-       // lo.showWindow();
-        Management ma =new Management();
+       Login lo= new Login("QUẢN LÍ CỬA HÀNG ĐỒ ĂN NHANH");
+       lo.showWindow();
         Connection conn = TestSQLConnection.getConnect("LAPTOP-0TRU1A0I\\NGOC", "RIN");
         if (conn != null) {
             System.out.println("Connected");
